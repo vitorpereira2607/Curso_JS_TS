9f37fde4d3a78114ed14e7da69a3afcd0183dc57
Vitor Pereira <vitor.pereira.09@hotmail.com>
2024-01-04 15:24:23 +0000

    js course introduction with console.log

---
+++ aula3/ex/exer.js
@@ -0,0 +1,5 @@
+//o numero 10 neste log é considerado um texto
+console.log('Meu nome é "Vitor". Estou aprendendo JavaScript às 14 da tarde');
+
+//o numero 10 neste log é do tipo number
+console.log('Meu nome é "Vitor". Estou aprendendo JavaScript às', 14, 'da tarde');
---
+++ aula3/index.js
@@ -0,0 +1,4 @@
+
+// `` -> estas aspas são utilizadas para template strings
+
+console.log(2, 15.9); // em js os numeros sao designados do tipo number, não existe a separação de valores como nas outras linguagens, neste caso, o 2 seria designado como tipo inteiro(INT) e o '15.9' como tipo float
