bc2c45546d62b2a9db85bc90e81dd6500738b637
vitorpereira267 <vitor.pereira.09@hotmail.com>
2024-04-24 14:51:11 +0100

    deleting duplicate folders

--- Chapter 10 Javascript + Node - Schedule Project(Express + Mongo)/scheduleproject/frontend/modules/FormValidation.js
+++
@@ -1,8 +0,0 @@
-export function createError(field, msg) {
-    const div = document.createElement('div');
-    div.innerHTML = msg;
-    div.classList.add('invalid-feedback');
-    div.style.marginBottom = '10px';
-    field.classList.add('is-invalid'); 
-    field.parentNode.appendChild(div); 
-}
